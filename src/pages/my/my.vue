15869f8976fa36a12a199f82d6edbe065aa63f31
StarsLeopad <1240056120@qq.com>
2024-01-07 22:52:58 +0800

    拦截器

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,33 +1,45 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useMemberStore } from '@/stores'
+import '@/utils/http'
 // console.log('aaaaaaa', useMemberStore)
 
 const memberStore = useMemberStore()
 
 const test = ref()
-uni.request({
-  url: 'https://www.example.com/request', //仅为示例，并非真实接口地址。
-  data: {
-    text: 'uni.request',
-  },
-  header: {
-    'custom-header': 'hello', //自定义请求头信息
-  },
-  success: (res) => {
-    console.log(res.data)
-    // text.value = 'request success'
-  },
-})
+// uni.request({
+//   url: 'https://www.example.com/request', //仅为示例，并非真实接口地址。
+//   data: {
+//     text: 'uni.request',
+//   },
+//   header: {
+//     'custom-header': 'hello', //自定义请求头信息
+//   },
+//   success: (res) => {
+//     console.log(res.data)
+//     // text.value = 'request success'
+//   },
+// })
+const getData = () => {
+  uni.request({
+    url: '/home/banner',
+    method: 'GET',
+    success: (res) => {
+      console.log('aaaa', res.data)
+      // text.value = 'request success'
+    },
+  })
+}
 </script>
 
 <template>
   <view class="my">
-    <view>会员信息：{{ memberStore.profile }}{{ test }}</view>
+    <view>会员信息：{{ memberStore.profile }}</view>
     <button
       @tap="
         memberStore.setProfile({
           nickname: '黑马先锋',
+          token: '23235',
         })
       "
       size="mini"
@@ -37,10 +49,11 @@ uni.request({
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
-    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">测试请求</button>
+    <button @tap="getData" size="mini" plain type="warn">测试请求</button>
   </view>
 </template>
 
 <style lang="scss">
 //
 </style>
+=
---
+++ src/utils/http.ts
@@ -0,0 +1,35 @@
+import { useMemberStore } from '@/stores'
+
+const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+
+const httpInterceptor = {
+  invoke(args: UniApp.RequestOptions) {
+    if (!args.url.startsWith('http')) args.url = baseURL + args.url
+
+    args.timeout = 10000
+
+    args.header = {
+      ...args.header,
+      'source-client': 'miniapp',
+    }
+
+    const memberStore = useMemberStore()
+    const token = memberStore.profile?.token
+    if (token) {
+      args.header.Authorization = token
+    }
+  },
+  success(args: any) {
+    // 请求成功后，修改code值为1
+    args.data.code = 1
+  },
+  fail(err: UniApp.RequestOptions) {
+    console.log('interceptor-fail', err)
+  },
+  complete(res: UniApp.RequestOptions) {
+    console.log('interceptor-complete', res)
+  },
+}
+
+uni.addInterceptor('request', httpInterceptor)
+uni.addInterceptor('uploadFile', httpInterceptor)
