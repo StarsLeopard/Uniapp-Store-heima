7f3b96a9ebd16d8931a2c4fb330cd582e7988ebc
徐鑫 <1240056120@qq.com>
2024-01-07 17:51:31 +0800

    git初始化

---
+++ src/pages/my/my.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+import { useMemberStore } from '@/stores'
+
+const memberStore = useMemberStore()
+
+// const test = ref()
+uni.request({
+  url: 'https://www.example.com/request', //仅为示例，并非真实接口地址。
+  data: {
+    text: 'uni.request',
+  },
+  header: {
+    'custom-header': 'hello', //自定义请求头信息
+  },
+  success: (res) => {
+    console.log(res.data)
+    // text.value = 'request success'
+  },
+})
+</script>
+
+<template>
+  <view class="my">
+    <view>会员信息：{{ memberStore.profile }}{{ test }}</view>
+    <button
+      @tap="
+        memberStore.setProfile({
+          nickname: '黑马先锋',
+        })
+      "
+      size="mini"
+      plain
+      type="primary"
+    >
+      保存用户信息
+    </button>
+    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">测试请求</button>
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
